5cbaf22a0b8a7edb311c1cc97d084282362757d1
paolahydra <patella.paola@gmail.com>
2021-04-11 19:09:36 +0200

    made the whiteBackground_blackSignal_rgbReference.m more general
    (renamed from whiteBackground_blackSignal_redReference.m )
    
    It can now show up-to-3 different channels as references in rgb over white background - user will select which channels, and in which color they should appear.
    The main signal channel remain black.

--- matlab_batchProcessing/whiteBackground_blackSignal_rgbReference.m
+++ matlab_batchProcessing/whiteBackground_blackSignal_rgbReference.m
@@ -6,26 +6,36 @@
 % written by Paola Patella, FMI - 2021
 
 
-%% define channel output
+%% define output channels
 blackCH = 2; % this channel will become black over white background
+
 redCH = 1; % this channel will become red over white background
+greenCH = 3; % use zero if you don't want any data shown in this channel
+blueCH = 2;  % use zero if you don't want any data shown in this channel
 
 % note that you may have to adjust values in the stretchlim function (for
 % each channel) depending on the quality of your own image
 
-%% interactively select files to process and do it
+
+%% interactively select files to process
 imagefiles = uipickfiles('Output', 'struct');
 
+
+%% do it
 for i = 1:length(imagefiles)
     fname_original = imagefiles(i).name;
     extension_i = strfind(imagefiles(i).name, '.');
     assert(~isempty(extension_i),'Is the file extension not specified?')
     extension_i = extension_i(end);
     fname_2save = [fname_original(1:extension_i-1), '_blackred', fname_original(extension_i:end)];
+       
+    %% load image
+    cdata = imread(fname_original); 
+    if ~isa(cdata, 'uint8')
+        cdata = im2uint8(cdata);
+    end
+    clear newcdata
     
-    %% load image(s)
-    cdata = imread(fname_original);
-
     %% split in channels
     for c = 1:size(cdata,3)
         ch(c).data = cdata(:,:,c);
@@ -34,22 +44,36 @@ for i = 1:length(imagefiles)
     
     %% convert black channel
     im = ch(blackCH).data;
-    lowhigh = stretchlim(im, [0.2 1]); % saturate the white a bit. Check that the values suit your images
+    lowhigh = stretchlim(im, [0.2 0.9999]); % saturate the white a bit. Check that the values suit your images
     im = imadjust(im,lowhigh);
     
     black_b = repmat(im,1,1,3);
     black_w = uint8(abs(double(black_b) - 255));
     
-    %% convert red channel
-    im = ch(redCH).data;
-    lowhigh = stretchlim(im, [0.6 1]);   % saturate the white in this channel! Check that the values suit your images
-    im = imadjust(im,lowhigh);
+    %% convert color channels
+    %first reorganize channels according to user directions
+    CH = [redCH, greenCH, blueCH];
+    for c = 1:size(cdata,3)
+        if CH(c)~=0
+            newcdata(:,:,c) = cdata(:,:,CH(c));
+        else
+            newcdata(:,:,c) = uint8(zeros(size(white)));
+        end
+    end
     
-    red_w_single = uint8(abs(double(im) - 255));
-    red_w = cat(3, white, red_w_single, red_w_single);
+    MC = zeros(size(newcdata));
+    for c = 1:size(newcdata,3)
+        im = newcdata(:,:,c);
+        lowhigh = stretchlim(im, [0.6 0.9999]);   % saturate the white in this channel! Check that the values suit your images
+        im = imadjust(im,lowhigh);
+        MC(:,:,setdiff(1:3, c)) = MC(:,:,setdiff(1:3, c)) + double(repmat(imcomplement(im),1,1,2));
+    end
+    MC = MC./2;
+    MC = uint8(MC);
+    M = min(MC, black_w);
+%     figure; imshow(M); 
     
-    %% fuse colors and save image
-    M = min(red_w, black_w);
+    %% save image
     imwrite(M, fname_2save);
 end
 
